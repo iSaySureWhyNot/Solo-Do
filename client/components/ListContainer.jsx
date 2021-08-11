f5f1c12e3483872cde6a4ab95067f7b69a87f70d
iSaySureWhyNot <isaysurewhynot@gmail.com>
2021-08-11 17:22:55 -0400

    Update ListContainer.jsx

--- client/components/ListContainer.jsx
+++ client/components/ListContainer.jsx
@@ -23,8 +23,36 @@ class ListContainer extends React.Component {
             })
             .catch(err => console.log('ListContainer.componentDidMount: get tasks: ERROR: ', err));
     }
+
+    componentDidUpdate() {
+        // fetch('/api/tasks', {
+        //     method: 'GET'
+        // })
+        //     .then(res => res.json())
+        //     .then((tasks) => {
+        //         if(!Array.isArray(tasks)) tasks = [];
+        //         return this.setState({
+        //             tasks
+        //         })
+        //     })
+        //     .catch(err => console.log('ListContainer.componentDidMount: get tasks: ERROR: ', err));
+    }
+
+    
     
     render(){
+
+        const deleteTask = () => {
+            fetch('api/tasks/delete', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'Application/JSON'
+                },
+                body: JSON.stringify({key: this.key})
+            })
+            console.log(this.key)
+        }
+
         if (!this.state) return (
             <div>
               <h1>Loading data, please wait...</h1>
@@ -43,7 +71,11 @@ class ListContainer extends React.Component {
         
             
         for(let i = 0; i < tasks.length; i++){
-            parsedTasks.push(<div key = {i}>{tasks[i]['content']}</div>)
+            parsedTasks.push(
+                <div key = {i}>{tasks[i]['content']}
+                    <button key = {'button'+i} onClick = {deleteTask}>
+                    </button>
+                </div>)
         }
             
         
